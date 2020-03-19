c4c56394e57fefbd44c3ab5454e280045bb48d1e
Sittisak <dev@sittisak.io>
2020-03-19 16:16:49 +0700

    Fix headers request not found

--- middleware/cookie-agent.js
+++ middleware/cookie-agent.js
@@ -1,18 +1,11 @@
-// export default function({ context, app }) {
-//   //   context.userAgent = process.server
-//   //     ? context.req.headers["user-agent"]
-//   //     : navigator.userAgent;
-
-//   console.log("middle worked!");
-//   console.log(context.req.headers["test_header"]);
-// }
-
-export default function({ app, context }) {
+export default function({ app, store, req }) {
   console.log("middle worked!");
 
-  const jwt = app.context.req.headers["kpc_jwt"];
-  if (jwt !== "undefined") {
-    app.$cookies.set("kpc_jwt", jwt);
-    console.log("kpc_jwt in header is " + jwt);
+  if (req && req.headers) {
+    const jwt = app.context.req.headers["kpc_jwt"];
+    if (jwt !== "undefined") {
+      app.$cookies.set("kpc_jwt", jwt);
+      console.log("kpc_jwt in header is " + jwt);
+    }
   }
 }
